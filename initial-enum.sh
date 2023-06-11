32c443573c253b85e8531d1eb8159b41cdb6f14b
SagaOfAGuy <68156940+SagaOfAGuy@users.noreply.github.com>
2023-06-11 13:25:54 -0400

    Update initial-enum.sh

--- initial-enum.sh
+++ initial-enum.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-# Script that pipes ports found from nmap into Autorecon if AutoRecon where to miss any ports
-
 # Check arguments
 if [ $# -eq 0 ]
   then
@@ -10,8 +8,4 @@ if [ $# -eq 0 ]
 fi
 
 # Function that performs enumeration
-function enum() {
-    sudo nmap -Pn -T5 -p- $1 -v | grep Discovered | awk -F " " '{print substr($4,1,length($4)-4)}' | tee ports.txt && tr '\n' , < ports.txt > ports_csv.txt && autorecon $1 -p `cat ports_csv.txt`
-}
-
-enum
+sudo nmap -Pn -T5 -p- $1 -v | grep Discovered | awk -F " " '{print substr($4,1,length($4)-4)}' | tee ports.txt && tr '\n' , < ports.txt > ports_csv.txt && autorecon $1 -p `cat ports_csv.txt`
