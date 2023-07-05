c7fae13a11cbf5488594689eedd586c5ce0f6217
SagaOfAGuy <bandoodle125@gmail.com>
2023-07-05 02:56:55 -0400

    Update

--- install.sh
+++ install.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
 
+# Exit script if user presses CTRL C
+trap "exit" INT
+
+# RED color outline
+RED="\e[31m"
+GREEN="\e[32m"
+ENDCOLOR="\e[0m"
+
 # Installing tools
 scripts=`ls -dl */ | awk -F " " '{print substr($9,1,length($9)-1)}'`
-for script in ${scripts[@]}; 
+for script in ${scripts[@]};
 do
-	printf "[+] Downloading $script to /usr/local/bin\n" 
+	printf "${GREEN}[+]${ENDCOLOR} Downloading $script to /usr/local/bin\n"
 	sudo cp $script/$script /usr/local/bin/$script
-
 done
