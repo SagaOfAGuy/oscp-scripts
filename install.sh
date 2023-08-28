c30346bebca4e7e07b89347a9fc99a563cea18b2
SagaOfAGuy <bandoodle125@gmail.com>
2023-08-27 20:34:47 -0400

    Update

--- install.sh
+++ install.sh
@@ -15,3 +15,6 @@ do
 	printf "${GREEN}[+]${ENDCOLOR} Downloading $script to /usr/local/bin\n"
 	sudo cp $script/$script /usr/local/bin/$script
 done
+
+# Install tools from initial-enum script
+sudo cp ./initial-enum/initial-enum-multi /usr/local/bin
